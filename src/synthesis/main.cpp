d844ac7256b033083f434160313d222ae566ce22
MarcoFavorito <marco.favorito@gmail.com>
2020-12-18 11:52:46 +0100

    make it possible to run only with MONA

--- src/synthesis/main.cpp
+++ src/synthesis/main.cpp
@@ -22,17 +22,34 @@ int main(int argc, char ** argv){
     string partfile;
     string autfile;
     string starting_player;
-    if(argc != 4){
-        cout<<"Usage: ./Syft DFAfile Partfile Starting_player(0: system, 1: environment)"<<endl;
-        return 0;
+    if(argc != 2 and argc != 4){
+      cout<<"Usage: ./Syft LTLFfile [Partfile Starting_player(0: system, 1: environment)]"<<endl;
+      return 0;
     }
     else{
         filename = argv[1];
-        partfile = argv[2];
-        starting_player = argv[3];
+        partfile = argc > 2? argv[2] : "";
+        starting_player = argc > 2? argv[3]: "";
     }
-    Cudd* mgr = new Cudd();
+
     autfile = get_DFAfile(filename);
+    clock_t c_mona_dfa_end = clock();
+    auto t_mona_dfa_end = chrono::high_resolution_clock::now();
+    std::cout << "DFA constructed by MONA CPU time used: "
+                << 1000.0 * (c_mona_dfa_end-c_start) / CLOCKS_PER_SEC << " ms\n"
+                << "DFA constructed by MONA wall clock time passed: "
+                << std::chrono::duration<double, std::milli>(t_mona_dfa_end-t_start).count()
+                << " ms\n";
+    if (argc < 3){
+      std::cout << "Total CPU time used: "
+                << 1000.0 * (c_mona_dfa_end-c_start) / CLOCKS_PER_SEC << " ms\n"
+                << "Total wall clock time passed: "
+                << std::chrono::duration<double, std::milli>(t_mona_dfa_end-t_start).count()
+                << " ms\n";
+      return 0;
+    }
+
+    Cudd* mgr = new Cudd();
     Syft::syn test(mgr, autfile, partfile);
     
     bool res = 0;
@@ -48,7 +65,6 @@ int main(int argc, char ** argv){
         cout<<"realizable"<<endl;
     else
         cout<<"unrealizable"<<endl;
-    
     clock_t c_end = clock();
     auto t_end = chrono::high_resolution_clock::now();
     std::cout << "Total CPU time used: "
